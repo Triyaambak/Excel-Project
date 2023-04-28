9961fbee42e8c56b205a8b095aef1db26fb3186c
Ayush <ayushsingh01042003@gmail.com>
2023-04-28 15:04:58 +0530

    Changed write

--- Write.java
+++ Write.java
@@ -1,7 +1,8 @@
 package com.demo.WriteInExcel;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-//import java.util.Scanner;
+import java.io.IOException;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -10,58 +11,50 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Write {
-	public static void write (int selections , String source , String entry , int start )
-	{
-		Cell cell;
-		String source_name = null;
-		int division = 0;		
-		try {
-			Workbook workbook = new XSSFWorkbook();
-			Sheet sheet = workbook.createSheet("Write In Excel");
-			String headername [] = {"Plot" , "Entry" , "Source" , "Remarks" , "Selections"};
-			Row row = sheet.createRow(0);
-			for(int i =0;i<headername.length;i++)
-			{
-				cell = row.createCell(i);
-				cell.setCellValue(headername[i]);
-			}
-			//System.out.print(selections);
-			//System.out.print("passed selection is "+selections);
-			//System.out.print("passed source is "+source);
-			//System.out.print("passed entry is "+entry);
-			//System.out.print("Start is "+start);
-			for(int i =1;i<=selections;i++)
-			{				
-				row = sheet.createRow(start++);	
-				for(int j =0;j<3;j++)
-				{
-					if(j==0)
-					{
-						cell = row.createCell(0);
-						cell.setCellValue(source);
-					}
-					if(j==1)
-					{
-						cell = row.createCell(1);
-						cell.setCellValue(entry+"-"+i);
-					}
-					if(j==2)
-					{
-						cell = row.createCell(2);
-						cell.setCellValue(source_name+":"+division+"-"+i);
-					}					
-				}
-			}							
-			FileOutputStream fileoutput = new FileOutputStream("/Java/Excel-Output.xlsx");
-			workbook.write(fileoutput);
-			fileoutput.close();
-			workbook.close();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		//sc.close();
-	}
+    public static void write(int selections, String source, String entry, int start) {
+        Cell cell;
+        String source_name = null;
+        int division = 0;
+        String filepath = "/Java/Excel-Output.xlsx";
+        try {
+            FileInputStream fileinput = new FileInputStream(filepath);
+            Workbook workbook = new XSSFWorkbook(fileinput);
+            Sheet sheet = workbook.getSheet("Write In Excel");
+            int lastRowNum = sheet.getLastRowNum();
+            String headername[] = { "Plot", "Entry", "Source", "Remarks", "Selections" };
+            if (lastRowNum == 0) {
+                // create header row if it doesn't exist
+                Row headerRow = sheet.createRow(0);
+                for (int i = 0; i < headername.length; i++) {
+                    cell = headerRow.createCell(i);
+                    cell.setCellValue(headername[i]);
+                }
+                lastRowNum++; // increment to start at data row
+            }
+            for (int i = 1; i <= selections; i++) {
+                Row row = sheet.createRow(lastRowNum++);
+                for (int j = 0; j < 3; j++) {
+                    if (j == 0) {
+                        cell = row.createCell(0);
+                        cell.setCellValue(source);
+                    }
+                    if (j == 1) {
+                        cell = row.createCell(1);
+                        cell.setCellValue(entry + "-" + i);
+                    }
+                    if (j == 2) {
+                        cell = row.createCell(2);
+                        cell.setCellValue(source_name + ":" + division + "-" + i);
+                    }
+                }
+            }
+            FileOutputStream fileoutput = new FileOutputStream(filepath);
+            workbook.write(fileoutput);
+            fileoutput.close();
+            workbook.close();
+            fileinput.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
-
